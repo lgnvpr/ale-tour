9cb99321a388180d0ce0eeaf639ceeea16e78124
lgnvpr <=>
2021-01-24 01:33:08 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,208 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Demo from "./container/Demo";
+import Tour, { ReactourStep } from "reactour";
+import styled, { createGlobalStyle } from "styled-components";
+import EditTour from "./container/EditTour";
 
+const tourConfig: ReactourStep[] = [
+	{
+		selector: '[data-tut="reactour__iso"]',
+		content: `Ok, let's start with the name of the Tour that is about to begin.`,
+		stepInteraction: true,
+		navDotAriaLabel: "abcxyz",
+		action: () => {
+			console.log("aaaa");
+		},
+	},
+	{
+		selector: '[data-tut="reactour__logo"]',
+		content: `And this is our cool bus...`,
+	},
+	{
+		selector: '[data-tut="reactour__copy"]',
+		content: `Keep in mind that you could try and test everithing during the Tour. 
+      For example, try selecting the highlighted text…`,
+	},
+	{
+		selector: '[data-tut="reactour__style"]',
+		content: () => <div>aaaaa</div>,
+		style: {
+			backgroundColor: "black",
+			color: "white",
+		},
+	},
+	{
+		selector: '[data-tut="reactour__goTo"]',
+		content:
+			"Probably you noted that the Tour scrolled directly to the desired place, and you could control the time also…",
+	},
+	{
+		selector: '[data-tut="reactour__position"]',
+		content:
+			"Probably you noted that the Tour scrolled directly to the desired place, and you could control the time also…",
+		position: "left",
+	},
+	{
+		selector: '[data-tut="reactour__scroll"]',
+		content:
+			"Probably you noted that the Tour scrolled directly to the desired place, and you could control the time also…",
+	},
+	{
+		selector: '[data-tut="reactour__scroll--hidden"]',
+		content: "Also when places are pretty hidden…",
+	},
+	{
+		selector: '[data-tut="reactour__action"]',
+		content:
+			"When arrived on each place you could fire an action, like… (look at the console)",
+		action: () =>
+			console.log(`
+                  ------------🏠🏚---------
+      🚌 Arrived to explore these beautiful buildings! 🚌
+                  ------------🏠🏚---------
+   🚧 This action could also fire a method in your Component 🚧
+    `),
+	},
+	{
+		selector: '[data-tut="reactour__state"]',
+		content:
+			"And the Tour could be observing changes to update the view, try clicking the button…",
+	},
+];
+
+export type TourType = {
+	top?: number;
+	left?: number;
+	title?: string;
+	index?: number;
+} & ReactourStep;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tourConfig, setTourConfig] = useState<TourType[]>([]);
+	const [isEdit, setEdit] = useState<boolean>(false);
+	const [isTest, setTest] = useState<boolean>(false);
+	document.onclick = (e) => {
+		const element: HTMLElement = e.target as any;
+		if (element.id === "edit-tour") {
+			setEdit(!isEdit);
+			element.innerHTML = isEdit ? "edit" : "done";
+			return;
+		}
+		if (isEdit) {
+			const path = getDomPath(element);
+			setTourConfig([
+				...tourConfig,
+				{
+					content: Math.random() * 1,
+					selector: path,
+					title: "3232",
+				},
+			]);
+		}
+	};
+
+	document.onmouseout = (e) => {
+		if (isEdit) {
+			const element: HTMLElement = e.target as any;
+			const getMyDiv = document.getElementById("myDiv");
+			element.style.background = `${getMyDiv?.style.background}`;
+			element.style.cursor = `${getMyDiv?.style.cursor}`;
+			getMyDiv?.remove();
+		}
+	};
+
+	document.onmouseover = (e) => {
+		const element: HTMLElement = e.target as any;
+		if (element.id === "edit-tour" || element.id === "test-tour") {
+			element.style.cursor = "default";
+			return;
+		}
+		if (isEdit) {
+			if (element) {
+				var myDiv = document.createElement("div");
+				myDiv.style.position = "absolute";
+				myDiv.style.left = `0px`;
+				myDiv.style.top = `0px`;
+				myDiv.style.height = `100%`;
+				myDiv.style.width = `100%`;
+				myDiv.style.border = "1px solid black";
+				myDiv.style.zIndex = "-1";
+				myDiv.style.background = element.style.background;
+				myDiv.style.cursor = element.style.cursor;
+				element.style.background = "rgba(219, 52, 235,0.3)";
+				element.style.cursor = "copy";
+				myDiv.id = "myDiv";
+				try {
+					element?.parentNode?.insertBefore(myDiv, element);
+				} catch {}
+			}
+		}
+	};
+
+	function getDomPath(el: any) {
+		if (!el) {
+			return;
+		}
+		var stack = [];
+		var isShadow = false;
+		while (el.parentNode != null) {
+			var sibCount = 0;
+			var sibIndex = 0;
+			for (var i = 0; i < el.parentNode.childNodes.length; i++) {
+				var sib = el.parentNode.childNodes[i];
+				if (sib.nodeName == el.nodeName) {
+					if (sib === el) {
+						sibIndex = sibCount;
+					}
+					sibCount++;
+				}
+			}
+			var nodeName = el.nodeName.toLowerCase();
+			if (isShadow) {
+				nodeName += "::shadow";
+				isShadow = false;
+			}
+			if (sibCount > 1) {
+				stack.unshift(
+					nodeName + ":nth-of-type(" + (sibIndex + 1) + ")"
+				);
+			} else {
+				stack.unshift(nodeName);
+			}
+			el = el.parentNode;
+			if (el.nodeType === 11) {
+				isShadow = true;
+				el = el.host;
+			}
+		}
+		stack.splice(0, 1);
+		return stack.join(" > ");
+	}
+
+	return (
+		<div className="App">
+			<button id="edit-tour">Edit</button>
+
+			<button id="test-tour" onClick={(e) => setTest(true)}>
+				Test
+			</button>
+
+			<Demo></Demo>
+			<Tour
+				steps={tourConfig}
+				isOpen={isTest}
+				maskClassName="mask"
+				className="helper"
+				rounded={5}
+				accentColor={"#5cb7b7"}
+				onRequestClose={() => {}}
+				getCurrentStep={(cu) => {
+					console.log(cu);
+				}}
+			/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/container/Demo.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+
+export default function Demo() {
+	return (
+		<div className="login_tour" id= "aaa">
+			<h2 tour-flynotes="1">
+				Weekly Coding Challenge #1: Sign in/up Form
+			</h2>
+			<div className="container" id="container">
+				<div className="form-container sign-up-container">
+					<form action="#">
+						<h1 tour-flynotes="5">Create Account</h1>
+						<div className="social-container">
+							<label className="social" tour-flynotes="2">
+								<i className="fab fa-facebook-f"></i>
+							</label >
+							<label className="social">
+								<i className="fab fa-google-plus-g"></i>
+							</label>
+							<label className="social">
+								<i className="fab fa-linkedin-in"></i>
+							</label>
+						</div>
+						<span>or use your email for registration</span>
+						<input type="text" placeholder="Name" />
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<button >Sign Up</button>
+					</form>
+				</div>
+				<div className="form-container sign-in-container">
+					<form action="#">
+						<h1 tour-flynotes="3">Sign in</h1>
+						<div className="social-container">
+							<label className="social" >
+								<i className="fab fa-facebook-f" ></i>
+							</label>
+							<label className="social">
+								<i className="fab fa-google-plus-g"></i>
+							</label>
+							<label className="social">
+								<i className="fab fa-linkedin-in"></i>
+							</label>
+						</div>
+						<span>or use your account</span>
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<label>Forgot your password?</label>
+						<button>Sign In</button>
+					</form>
+				</div>
+				<div className="overlay-container">
+					<div className="overlay">
+						<div className="overlay-panel overlay-left">
+							<h1>Welcome Back!</h1>
+							<p>
+								To keep connected with us please login with your
+								personal info
+							</p>
+							<button className="ghost" id="signIn">
+								Sign In
+							</button>
+						</div>
+						<div className="overlay-panel overlay-right">
+							<h1>Hello, Friend!</h1>
+							<p>
+								Enter your personal details and start journey
+								with us
+							</p>
+							<button className="ghost" id="signUp" tour-flynotes="9" onClick = {()=>{
+							console.log("aaa")
+						}}>
+								Sign Up
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<footer>
+				<p>
+					Created with <i className="fa fa-heart"></i> by
+					<label>Florin Pop</label>- Read how I created this and how
+					you can join the challenge
+					<label>here</label>.
+				</p>
+			</footer>
+		</div>
+	);
+}
